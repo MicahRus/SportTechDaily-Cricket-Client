98e87c929fc0a54bfeb7cd6adf073e565df469e1
Micah <Rus.Micah@gmail.com>
2020-08-11 13:41:56 +1000

    Stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -116,6 +116,69 @@ class Home extends React.Component {
     // this.getPlayerData()
   };
 
+  renderScatterPlot = () => {
+    return (
+      <div style={{ height: "950px" }}>
+        <ResponsiveScatterPlot
+          data={this.state.data}
+          margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
+          xScale={{ type: "linear", min: 0, max: "auto" }}
+          xFormat={function (e) {
+            return e + " kg";
+          }}
+          yScale={{ type: "linear", min: 0, max: "auto" }}
+          yFormat={function (e) {
+            return e + " cm";
+          }}
+          blendMode="multiply"
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            orient: "bottom",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "weight",
+            legendPosition: "middle",
+            legendOffset: 46,
+          }}
+          axisLeft={{
+            orient: "left",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "size",
+            legendPosition: "middle",
+            legendOffset: -60,
+          }}
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 130,
+              translateY: 0,
+              itemWidth: 100,
+              itemHeight: 12,
+              itemsSpacing: 5,
+              itemDirection: "left-to-right",
+              symbolSize: 12,
+              symbolShape: "circle",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemOpacity: 1,
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
+    );
+  };
+
   renderRadar = () => {
     return (
       <div style={{ height: "1000px" }}>
@@ -337,9 +400,10 @@ class Home extends React.Component {
     return (
       <>
         {this.renderControlBar()}
-        {/* {this.renderButton()} */}
-        {/* {this.renderModal()} */}
+        {this.renderButton()}
+        {this.renderModal()}
         {this.renderRadar()}
+        {/* {this.renderScatterPlot()} */}
         {this.renderAdvancedOptions()}
       </>
     );
