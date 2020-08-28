44e48b27fbca8d00205b810e09890fd7184562a4
tea-jean <tjholt@live.com.au>
2020-08-28 13:55:16 +1000

    ats, fts and dfs tables

--- src/components/FantasySport.js
+++ src/components/FantasySport.js
@@ -3,11 +3,86 @@ import { Redirect } from "react-router-dom";
 import { Form, Row, Col, Button, Table } from "react-bootstrap";
 
 class FantasySport extends React.Component {
-  state = { redirect: null };
+  state = { 
+    redirect: null,
+    dfs_summary: [],
+  };
 
   componentDidUpdate() {
       console.log("true");
       console.log(this.state);
+  };
+
+  componentDidMount() {
+    this.getDfsData();
+  };
+
+  getDfsData = async () => {
+    console.log("THIS IS A LOG")
+    const response = await fetch("http://localhost:3001/dfs_summary");
+    const data = await response.json();
+    console.log("Here is the data:", data);
+    this.setState({ dfs_summary: data.rows });
+  };
+
+  // renderDfsMatchSelect() {
+  //   return(
+
+  //     <Form>
+  //           <Form title="match-select">
+  //               <Row>
+  //                   <Col>
+  //                       <Form.Group>
+  //                           <Form.Label>Match</Form.Label>
+  //                           <Form.Control as="select">
+  //                               <option></option>
+  //                           </Form.Control>
+  //                       </Form.Group>
+  //                   </Col>
+  //               </Row>
+  //           </Form>
+  //           <Button>
+  //               Go!
+  //           </Button>
+  //     </Form>
+  //   )
+  // }
+
+  renderDfsTable() {
+    return(
+      <div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>Expected Minutes</th>
+              <th>Price/Pred</th>
+              <th>OS Prev Rd</th>
+              <th>DS Price</th>
+              <th>Match</th>
+              <th>Team</th>
+              <th>Position</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.dfs_summary.map((item) => {
+              return(
+                <tr>
+                  <td>{item.player}</td>
+                  <td>{item.minutes}</td>
+                  <td>{item.price_pred}</td>
+                  <td>{item.os_prev}</td>
+                  <td>{item.ds_price}</td>
+                  <td>{item.match_name}</td>
+                  <td>{item.team}</td>
+                  <td>{item.pos}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+      </div>
+    )
   }
   render() {
       if (this.state.redirect) {
@@ -19,59 +94,7 @@ class FantasySport extends React.Component {
                 <h1>Fantasy Sports DFS</h1>
             </div>
             <div>
-                <Form>
-                    <Form title="match-select">
-                        <Row>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label>Match</Form.Label>
-                                    <Form.Control as="select">
-                                        <option></option>
-                                    </Form.Control>
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                    </Form>
-                    <Form title="market-select">
-                        <Row>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label>Market</Form.Label>
-                                    <Form.Control as="select">
-                                        <option>DFS</option>
-                                    </Form.Control>
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                    </Form>
-                    <Button>
-                        Go!
-                </Button>
-                </Form>
-            </div>
-            <div>
-            <Table responsive="sm">
-                <thead>
-                  <tr>
-                    <th>Player</th>
-                    <th>DFS</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Player Name</td>
-                    <td>Table cell</td>
-                  </tr>
-                  <tr>
-                    <td>Player Name</td>
-                    <td>Table cell</td>
-                  </tr>
-                  <tr>
-                    <td>Player Name</td>
-                    <td>Table cell</td>
-                  </tr>
-                </tbody>
-              </Table>
+              {this.renderDfsTable()}
             </div>
         </div>
     )
--- src/components/SportsBetting.js
+++ src/components/SportsBetting.js
@@ -7,9 +7,10 @@ class SportsBetting extends React.Component {
 
     state = { 
       redirect: null,
-      market: null,
+      market: "ATS",
       match: null,
       ats_summary: [],
+      fts_summary: [],
     };
 
     componentDidUpdate() {
@@ -18,6 +19,7 @@ class SportsBetting extends React.Component {
     }
     componentDidMount() {
       this.getAtsData();
+      this.getFtsData();
     }
 
     // Retrieves data from ats_summary table
@@ -29,46 +31,63 @@ class SportsBetting extends React.Component {
       this.setState({ ats_summary: data.rows });
     };
 
+    getFtsData = async () => {
+      console.log("THIS IS A LOG")
+      const response = await fetch("http://localhost:3001/fts_summary");
+      const data = await response.json();
+      console.log("Here is the FTS data:", data);
+      this.setState({ fts_summary: data.rows });
+    };
+
     // marketButtonSelectHandler = (event) => {
     //   let marketOption = event.target.value;
 
-    //   this.
-    // }
+    //   this.market(marketOption);
+    // };
 
-    matchButtonSelectHandler = (event) => {
-      let matchName = event.target.value;
+    // matchButtonSelectHandler = (event) => {
+    //   let matchName = event.target.value;
 
-      this.getAtsData(matchName);
-    };
+    //   this.getAtsData(matchName);
+    // };
     
     // Retrieves player odds data, by match
-    getPlayersByMatch = async (matchName) => {
-      const response = await fetch(`http://localhost:3001/ats_summary/match?matchName=${matchName}`);
-      const data = await response.json();
-      this.setState({ match_names: data.rows })
-    };
+    // getPlayersByMatch = async (matchName) => {
+    //   const response = await fetch(`http://localhost:3001/ats_summary/match?matchName=${matchName}`);
+    //   const data = await response.json();
+    //   this.setState({ match_names: data.rows })
+    // };
 
-    render() {
-        if (this.state.redirect) {
-            return <Redirect to={this.state.redirect} />;
-        }
+    renderMarketSelect() {
+      const marketOptions = [
+        { value: "ATS", label: "ATS"},
+        { value: "FTS", label: "FTS"},
+      ];
+      return(
+        <div>
+          <Select
+            options={marketOptions}
+            onChange={(e) => {
+              this.setState({
+                market: e.value,
+              });
+            }}
+            placeholder={marketOptions[0].label}
+          />
+        </div>
+      )
+    }
+    
+    renderAtsTable() {
+        // if (this.state.redirect) {
+        //     return <Redirect to={this.state.redirect} />;
+        // }
         return (
             <div>
                 <div>
-                    <h2>Check the Odds</h2>
+                    <h2>ATS Odds</h2>
                 </div>
-                <div>
-                  <Select
-                    options={this.state.options}
-                    onChange={(e) => {
-                      this.setState({
-                        barStat1: [e.value],
-                        refreshBarChart: true,
-                      });
-                    }}
-                    placeholder={this.state.barStat1[0]}
-                  />
-                </div>
+
                 {/* <Form inline>
                   <Form title="market">
                     <Row>
@@ -85,14 +104,11 @@ class SportsBetting extends React.Component {
                               <option value="ftsOption">
                                 FTS
                               </option>
-                              <option value="dtsOption">
-                                DTS
-                              </option>
                         </Form.Control>
                       </Col>
                     </Row>
                   </Form> */}
-                  <Form title="match">
+                  {/* <Form title="match">
                     <Row>
                       <Col>
                         <Form.Label> Match </Form.Label>
@@ -111,7 +127,7 @@ class SportsBetting extends React.Component {
                         </Form.Control>
                       </Col>
                     </Row>
-                  </Form>
+                  </Form> */}
                 {/* </Form> */}
                 <div>
                   <Table striped bordered hover size="sm">
@@ -123,39 +139,127 @@ class SportsBetting extends React.Component {
                         <th>Neds</th>
                         <th>Pointsbet</th>
                         <th>Topsport</th>
-                        <th>ATS Empirical</th>
+                        <th>Highest</th>
+                        <th>ATS Historical</th>
                         <th>ATS Model</th>
+                        <th>Highest/Historical</th>
+                        <th>Highest/Model</th>
+                        <th>Match</th>
                         <th>Team</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                      </tr>
-                      <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                      </tr>
+                      {this.state.ats_summary.map((item) => {
+                        return(
+                          <tr>
+                            <td>{item.player}</td>
+                            <td>{item.sb}</td>
+                            <td>{item.beteasy}</td>
+                            <td>{item.neds}</td>
+                            <td>{item.pointsbet}</td>
+                            <td>{item.topsport}</td>
+                            <td>{item.highest}</td>
+                            <td>{item.ats_empirical}</td>
+                            <td>{item.ats_model}</td>
+                            <td>{item.high_emp}</td>
+                            <td>{item.high_mod}</td>
+                            <td>{item.match_name}</td>
+                            <td>{item.team}</td>
+                          </tr>
+                        )
+                      })}
                     </tbody>
                   </Table>
                 </div>
             </div>
         )
+    };
+
+    renderFtsTable() {
+      return (
+          <div>
+              <div>
+                  <h2>FTS Odds</h2>
+              </div>
+              <div>
+                <Table striped bordered hover size="sm">
+                  <thead>
+                    <tr>
+                      <th>Player</th>
+                      <th>SB</th>
+                      <th>Beteasy</th>
+                      <th>Neds</th>
+                      <th>Pointsbet</th>
+                      <th>Topsport</th>
+                      <th>Highest</th>
+                      <th>FTS Historical</th>
+                      <th>FTS Model</th>
+                      <th>Highest/Historical</th>
+                      <th>Highest/Model</th>
+                      <th>Match</th>
+                      <th>Team</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.fts_summary.map((item) => {
+                      return(
+                        <tr>
+                          <td>{item.player}</td>
+                          <td>{item.sb}</td>
+                          <td>{item.beteasy}</td>
+                          <td>{item.neds}</td>
+                          <td>{item.pointsbet}</td>
+                          <td>{item.topsport}</td>
+                          <td>{item.highest}</td>
+                          <td>{item.ats_empirical}</td>
+                          <td>{item.ats_model}</td>
+                          <td>{item.high_emp}</td>
+                          <td>{item.high_mod}</td>
+                          <td>{item.match_name}</td>
+                          <td>{item.team}</td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </Table>
+              </div>
+          </div>
+      )
+  }
+
+    render() {
+      if (this.state.redirect) {
+        return <Redirect to={this.state.redirect} />;
+    }
+      if (this.state.market === "ATS") {
+        return(
+          <>
+            <div>
+              <h2>Check the Odds</h2>
+            </div>
+            <div>
+              {this.renderMarketSelect()}
+            </div>
+            <div>
+              {this.renderAtsTable()}
+            </div>
+          </>
+        )
+      } else {
+        return(
+          <>
+            <div>
+              <h2>Check the Odds</h2>
+            </div>
+            <div>
+              {this.renderMarketSelect()}
+            </div>
+            <div>
+              {this.renderFtsTable()}
+            </div>
+          </>
+        )
+      }
     }
 }
 
