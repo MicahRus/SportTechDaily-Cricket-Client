f99758a5f8cbc61f46b89fb0a50cf1d6e1f9a6e0
Micah <Rus.Micah@gmail.com>
2020-09-02 12:22:11 +1000

    Added select and filtering for teams

--- src/components/FantasySport.js
+++ src/components/FantasySport.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import { Table, OverlayTrigger } from "react-bootstrap";
 
+import Select from "react-select";
+
 import OwnershipPopover from "./Popovers/Ownership";
 import PredictedScorePopover from "./Popovers/PredictedScore";
 import PricePredPopover from "./Popovers/PricePred";
@@ -20,15 +22,113 @@ class FantasySport extends React.Component {
     this.getDfsData();
   }
 
+  setMatchData = () => {
+    const atsMatchArray = [];
+    let matchNames = [{ value: "All Teams", label: "All Teams" }];
+
+    this.state.dfs_summary.map((item) => atsMatchArray.push(item.match_name));
+
+    let cleanMatchArray = [...new Set(atsMatchArray)];
+
+    cleanMatchArray.map((match) => {
+      matchNames.push({ value: match, label: match });
+    });
+
+    this.setState({ matchNames });
+  };
+
+  renderTeamSelect = () => {
+    return (
+      <div>
+        <Select
+          options={this.state.matchNames}
+          onChange={(e) =>
+            this.setState({ selectedMatch: e.value }, () => {
+              this.setFilteredTable();
+            })
+          }
+          placeholder={"Choose a team"}
+        />
+      </div>
+    );
+  };
+
+  setFilteredTable = () => {
+    let filteredMatches = [];
+    this.state.dfs_summary.map((item) => {
+      if (item.match_name === this.state.selectedMatch) {
+        filteredMatches.push(item);
+      }
+    });
+    this.setState({ filteredMatches });
+  };
+
   getDfsData = async () => {
     const response = await fetch(
       `${process.env.REACT_APP_BACKEND_URL}/dfs_summary`
     );
     const data = await response.json();
-    this.setState({ dfs_summary: data.rows });
+    this.setState({ dfs_summary: data.rows }, () => {
+      this.setMatchData();
+    });
   };
 
-  renderDfsTable() {
+  filteredDfsTable = () => {
+    return (
+      <div className="tableFixHead">
+        <Table bordered striped hover>
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>Draftstars Price</th>
+              <OverlayTrigger
+                placement="bottom"
+                trigger={["focus", "hover"]}
+                overlay={PredictedScorePopover}
+              >
+                <th>Predicted Score</th>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="bottom"
+                trigger={["focus", "hover"]}
+                overlay={PricePredPopover}
+              >
+                <th>Price/Pred (%)</th>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="bottom"
+                trigger={["focus", "hover"]}
+                overlay={OwnershipPopover}
+              >
+                <th>Ownership (%) Previous Round</th>
+              </OverlayTrigger>
+              <th>Match</th>
+              <th>Team</th>
+              <th>Position</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.filteredMatches?.map((item) => {
+              return (
+                <tr>
+                  <td>{item.player}</td>
+                  <td>{Math.round(item.ds_price)}</td>
+                  <td>{Math.round(item.price_pred)}</td>
+                  <td>{Math.round(item.os_prev)}</td>
+                  <td>{Math.round(item.ds_price)}</td>
+                  <td>{item.match_name}</td>
+                  <td>{item.team}</td>
+                  <td>{item.pos}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    );
+  };
+
+  dfsTable() {
     return (
       <div className="tableFixHead">
         <Table bordered striped hover>
@@ -67,7 +167,7 @@ class FantasySport extends React.Component {
               return (
                 <tr>
                   <td>{item.player}</td>
-                  <td>{Math.round(item.minutes)}</td>
+                  <td>{Math.round(item.ds_price)}</td>
                   <td>{Math.round(item.price_pred)}</td>
                   <td>{Math.round(item.os_prev)}</td>
                   <td>{Math.round(item.ds_price)}</td>
@@ -82,16 +182,25 @@ class FantasySport extends React.Component {
       </div>
     );
   }
+
+  renderTable = () => {
+    if (!this.state.selectedMatch || this.state.selectedMatch === "All Teams") {
+      return this.dfsTable();
+    }
+    return this.filteredDfsTable();
+  };
+
   render() {
     if (this.state.redirect) {
       return <Redirect to={this.state.redirect} />;
     }
     return (
       <div>
+        <div>{this.renderTeamSelect()}</div>
         <div>
           <h1>Daily Fantasy Sports</h1>
         </div>
-        <div>{this.renderDfsTable()}</div>
+        <div>{this.renderTable()}</div>
       </div>
     );
   }
